613bd298bb54f3bd3e8998cbca3eb83a045cad4e
Pressy444 <145189170+Pressy444@users.noreply.github.com>
2023-12-04 14:07:21 +0100

    prescillia change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import Root from './routes/root.jsx'
 
 import Home from './pages/accueil.jsx'
 import Page1 from './pages/page1.jsx'
-import Gallerie from './pages/gallerie.jsx'
+import Pressy from './pages/gallerie.jsx'
 
 import './index.css'
 import '@fontsource/roboto/300.css';
@@ -32,7 +32,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/gallerie",
-        element: <Gallerie />,
+        element: <Pressy />,
       }
     ]
   },
---
+++ frontend/src/pages/gallerie.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const gallerie = () => {
+  return (
+    <div>gallerie</div>
+  )
+}
+
+export default gallerie
