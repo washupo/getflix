4f24c2ef5614dcc7bd0e964750b21d87e64137cf
Pauline Washukenyi <paulinewashukenyi@gmail.com>
2023-12-04 21:51:15 +0100

    update

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -26,7 +26,7 @@ const HeroSection = () => {
 
 
 {/*buttons*/}
-<Link to='page1' className= 'bg-blue-500 text-white px-4 py-2 mr-4'>S'INSCRIRE</Link>
+<Link to='login' className= 'bg-blue-500 text-white px-4 py-2 mr-4'>S'INSCRIRE</Link>
 <button className= 'bg-gray-500 text-gray-700 px-4 py-2'>SE CONNECTER</button>
 </div>
 
