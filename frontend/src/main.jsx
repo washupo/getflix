c3ddd27775c22f818f745a35f54bbe9a1aea7a41
Julie Dussaussois <j.dussaussois@gmail.com>
2023-12-04 09:59:37 +0100

    routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Root from './routes/root.jsx'
+import Home from './pages/accueil.jsx'
+import Page1 from './pages/page1.jsx'
+import Gallerie from './pages/gallerie.jsx'
 import './index.css'
+
+import {
+  createBrowserRouter, RouterProvider
+} from "react-router-dom"
+import { ConfigProvider } from 'antd'
+
+const theme = {
+  token: {
+    colorPrimary: '#724c89',
+    borderRadius: 2,
+    colorBgContainer: '#f6ffed',
+  },
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <div>404</div>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/page1",
+        element: <Page1 />,
+      },
+      {
+        path: "/gallerie",
+        element: <Gallerie />,
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <ConfigProvider theme={theme}>
+      <RouterProvider router={router} />
+     </ConfigProvider>
   </React.StrictMode>,
 )
---
+++ frontend/src/pages/accueil.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+import 'tailwindcss/tailwind.css';
+
+const Home = () => {
+  return (
+    <div>
+      
+      <HeroSection />
+
+    </div>
+  )
+}
+
+export default Home
