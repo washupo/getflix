b8e4966d129361bbe8086d668c790a0db6e4b156
Pauline Washukenyi <paulinewashukenyi@gmail.com>
2023-12-05 22:09:46 +0100

    Correction Middleware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,6 +1,7 @@
 // Middleware d'authentification
 require('dotenv').config(); // load environment variables
 const jwt = require('jsonwebtoken');
+const { UserModel } = require('../models/User');
 
 const jwtSecret = process.env.SECRET_KEY || 'defaultSecret'; // Use the JWT secret from the environment variable
 
@@ -19,43 +20,12 @@ function authMiddleware(req, res, next) {
 
     // Ajouter l'utilisateur décodé à la requête pour une utilisation ultérieure
     req.user = decoded.user;
+    // Passer à la prochaine étape du middleware
     next();
   } catch (err) {
-    res.status(401).json({ message: 'Jetons invalide.' });
+    return res.status(401).json({ message: 'Accès non autorisé. Jetons invalide.' });
   }
 }
 
+// Exposer les fonctions nécessaires
 module.exports = authMiddleware;
-
-
-// //Gestion de l'authentification
-// const { UserModel } = require('../models/User');
-
-
-
-// // Middleware d'authentification
-// function authenticate(req, res, next) {
-//   // Récupérer le token depuis l'en-tête Authorization
-//   const token = req.header('Authorization');
-
-//   // Vérifier si le token existe
-//   if (!token) {
-//     return res.status(401).json({ message: 'Accès non autorisé. Token manquant.' });
-//   }
-
-//   try {
-//     // Vérifier et décoder le token
-//     const decodedToken = jwt.verify(token, 'votreCleSecrete');
-
-//     // Ajouter les données utilisateur au objet de requête pour une utilisation ultérieure
-//     req.userId = decodedToken.userId;
-
-//     // Passer à la prochaine étape du middleware
-//     next();
-//   } catch (error) {
-//     return res.status(401).json({ message: 'Accès non autorisé. Token invalide.' });
-//   }
-// }
-
-// // Exposer les fonctions nécessaires
-// module.exports = authenticate;
