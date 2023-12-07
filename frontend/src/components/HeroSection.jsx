93c09e2ca9fbb429b4d400369ea88194ad2acc5f
Pressy444 <145189170+Pressy444@users.noreply.github.com>
2023-12-07 09:52:19 +0100

    prescillia 7
    JIJIHI

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,48 +1,149 @@
-import React from 'react'
-import background from '../assets/banner.webp'
-import logo from '../assets/logo.webp'
+import React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const HeroSection = () => {
+
+
+
+
+
+
+
+
+
+
+function Copyright(props) {
   return (
-
-
-   <div
-  className="hero min-h-screen flex flex-col items-center justify-center space-y-4"
-  style={{
-    backgroundImage: `url(${background})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  }}
->
-  {/* Contenu principal */}
-  <img src={logo} className="w-25 h-25" />
-
-  <div className="text-center text-white">
-    <h1 className="font-dhurjati text-2xl lg:text-3xl xl:text-4xl mx-auto mt-8 shadow-neon">
-      Films et séries en illimité, et bien plus<br />
-      Où que vous soyez.
-    </h1>
-    <p className="font-dhurjati mx-auto mt-4 shadow-neon">Annulez à tout moment.</p>
-
-    {/* Boutons avec espace en haut et en bas */}
-    <div className="py-4 items-center justify-center space-y-4">
-      <button className="px-7 py-2 mx-auto text-3xl rounded-xl bg-indigo-500 text-white shadow-lg shadow-indigo-500/50 transition ease-in-out duration-300 transform hover:-translate-y-1 hover:scale-110 hover:bg-red-500">
-        S'INSCRIRE
-      </button>
-
-      <button className="px-7 py-2 mx-auto text-3xl rounded-xl bg-cyan-500 text-white shadow-lg shadow-cyan-500/50 transition ease-in-out duration-300 transform hover:-translate-y-1 hover:scale-110 hover:bg-red-500">
-        CONNECTION
-      </button>
-    </div>
-  </div>
-</div>
- 
-   
-
-
-
-    
-  )
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default HeroSection
+const defaultTheme = createTheme();
+
+export default function SignInSide() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: '100vh', widht: '100vh', justifyContent: 'center'}}>
+
+        <CssBaseline />
+        <Grid
+          item
+          xs={12}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://pics.craiyon.com/2023-09-14/6bb75488481c43bfb4590dfbcf35c96d.webp)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          {/* Vous pouvez ajouter du contenu ici si nécessaire */}
+        </Grid>
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 11,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              bgcolor: '#9A1665',
+              color: 'black',
+              padding: '0',
+              boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+              borderRadius: '7px'
+              
+            }}
+          >
+            <Avatar sx={{ m: 1,  bgcolor: 'black' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5" >
+              Sign in
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1, margin: 4}}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2, bgcolor: 'black', color: 'white', width: '50%' }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
