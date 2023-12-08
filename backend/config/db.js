83c660bd4ff9ee64b1be1b5751b95a3aca6cd4f6
neb244 <ezaabouji.zineb@outlook.com>
2023-12-08 10:19:27 +0100

    ajout

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,27 +1,14 @@
-//Configuration de la base de donnée
-// Path: backend/config/db.js
-const mongoose = require('mongoose');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
-// URL de connexion à votre base de données MongoDB
 const dbUrl = process.env.MONGODB_URI;
 
-// Connexion à la base de données
-mongoose.connect(dbUrl);
+mongoose.connect(dbUrl)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
-// Gérer les événements de connexion
-const db = mongoose.connection;
-
-db.on('error', (err) => {
-  console.error('Erreur de connexion à MongoDB :', err);
-});
-
-db.once('open', () => {
-  console.log('Connexion à MongoDB réussie');
-});
-
-// Exporter l'objet de connexion pour être utilisé dans d'autres parties de l'application si nécessaire
-module.exports = {
-  db,
-  url: dbUrl,
-};
+module.exports = mongoose.connect;
