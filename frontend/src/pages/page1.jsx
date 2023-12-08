8b58b64d0238c10e3a065b1921ecc031ebfa6714
neb244 <ezaabouji.zineb@outlook.com>
2023-12-08 14:51:25 +0100

    page d'acceuil

--- frontend/src/pages/page1.jsx
+++ frontend/src/pages/page1.jsx
@@ -1,100 +1,24 @@
-import React from 'react'
-import Button from '@mui/material/Button';
+import React from 'react';
 import ResponsiveAppBar from '../components/appbar';
-import { Grid, Paper, Box, Avatar, Card, CardContent, CardHeader, CardMedia, IconButton, Typography } from '@mui/material';
-import { red } from '@mui/material/colors';
+import { Grid } from '@mui/material';
+import Movie from '../components/movie';
 
-//// theme provider pour le theme couleur //////////
 const Mynav = () => {
-  return (
-    <div>
-      {/* <Button color="primary">Bouton principal</Button>
-      <Button color="secondary">Bouton secondaire</Button> */}
-      {/* <ColorTabs /> Intégration du composant ColorTabs */}
-    </div>
-  );
+    return (
+        <div>
+            {/* ... */}
+        </div>
+    );
 };
 
-const affiches = [
-  {
-    titre: 'Film 1',
-    image: 'https://placekitten.com/345/194'
-  },
-  {
-    titre: 'Film 2',
-    image: 'https://placekitten.com/345/194'
-  },
-  {
-    titre: 'Film 3',
-    image: 'https://placekitten.com/345/194'
-  }
-]
-
 const Page1 = () => (
-  <div> 
-    <ResponsiveAppBar />
-    <Mynav />
-    
-    <Grid sx={{ flexGrow: 1 }} container spacing={2}>
-      <Grid item xs={12}>
-        <Grid container justifyContent="center" spacing={2}>
-          {affiches.map((film) => (
-            <Grid key={film} item>
-           
-              <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-          </IconButton>
-        }
-        title={film.titre}
-        subheader="2016"
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        image={film.image}
-        alt="chatton"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-         
-        </Typography>
-      </CardContent>
-     
-      
-    </Card>
-            </Grid>
-          ))}
+    <div>
+        <ResponsiveAppBar />
+        <Mynav />
+        <Grid container justifyContent="center">
+            <Movie /> {/* Utilisation du composant Movie ici */}
         </Grid>
-        <Box sx={{ m: 0.5 }} />
-        <Grid container justifyContent="center" spacing={2}>
-          {[0, 1, 2, 4, 5].map((value) => (
-            <Grid key={value} item>
-              <Paper
-                sx={{
-                  height: 140,
-                  width: 100,
-                  backgroundColor: (theme) =>
-                    theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-                }}
-              />
-             
-            </Grid>
-          ))}
-           <div>
-              <Button color="primary">More films</Button>
-              </div>
-        </Grid>
-      </Grid>
-    </Grid>
-  </div>
-          );
+    </div>
+);
 
-export default Page1
-
+export default Page1;
