cd19fab222c7b49c34adac7f7e071833e4da25b1
Pauline Washukenyi <paulinewashukenyi@gmail.com>
2023-12-18 10:08:38 +0100

    Package + server.js

--- backend/server.js
+++ backend/server.js
@@ -10,17 +10,38 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-//Initalizing the express app
-require("./routes/movies")(
+//Importing the connectToDB function to the index.js file as it is the main entry to the project + calling the function or running the function
+const connectToDB = require("./config/db");
+connectToDB();
+
+//Adding Node features
+// Middleware pour parser les données JSON dans les requêtes
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
   app.use(
     cors({
-      origin: "http://localhost:5173", // Update with your React app's URL
+      origin: '*', // Update with your React app's URL
       credentials: true, // Enable credentials (cookies)
       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
       allowedHeaders: "Content-Type, Authorization",
     })
   )
-);
+
+// //Initalizing the express app
+// require("./routes/movies")(
+//   app.use(
+//     cors({
+//       origin: "http://localhost:5173", // Update with your React app's URL
+//       credentials: true, // Enable credentials (cookies)
+//       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//       allowedHeaders: "Content-Type, Authorization",
+//     })
+//   )
+// );
+
+// Middleware pour parser les cookies
+app.use(cookieParser());
 
 //Importing the auth routes module
 const auth = require("./routes/authRoutes");
@@ -41,17 +62,46 @@ const options = {
 
 const server = http.createServer(options, app);
 
-//Importing the connectToDB function to the index.js file as it is the main entry to the project + calling the function or running the function
-const connectToDB = require("./config/db");
-connectToDB();
+// //const app = express();
+// const fetch = require('node-fetch');
 
-//Adding Node features
-// Middleware pour parser les données JSON dans les requêtes
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+// // Fetch popular movies from TMDB
+// const fetchMovies = async (page = 1) => {
+//   try {
+//     const url = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.MOVIE_DB_API_KEY}&page=${page}`;
+//     const options = {
+//       method: 'GET',
+//       headers: {
+//         accept: 'application/json',
+//         Authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxODlmMzQ2NDlmMDBlMTMxYzBkYzAxYTkwMjhkYjY4ZCIsInN1YiI6IjY1NzA4NTllNzlhMWMzMDBlMThlN2U2ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6WbmzdN4CKbAjO7tpCS9dGmrmy2stUwRFDmxaPW1MbA' // Remplace par ton jeton JWT
+//       }
+//     };
 
-// Middleware pour parser les cookies
-app.use(cookieParser());
+//     const response = await fetch(url, options);
+//     const data = await response.json();
+//     return data.results;
+//   } catch (error) {
+//     console.error(error);
+//     return [];
+//   }
+// };
+
+// // Routes
+// app.get('/movies', async (req, res, next) => {
+//   try {
+//     const { page } = req.query;
+//     const data = await fetchMovies(page);
+
+//     return res.status(200).json({
+//       status: 200,
+//       message: `${data.length} movies found`,
+//       data,
+//     });
+//   } catch (err) {
+//     return next(err);
+//   }
+// });
+
 
 // Importing the development support form utils/development.js
 const { printConsole } = require("./utils/development");
@@ -65,6 +115,14 @@ running on the local macchine we are asking the app to use 3000 as the port numb
 const PORT = process.env.PORT || 8000;
 
 //Listing to the app and running it on PORT 8000
-server.listen(PORT, () => {
-  console.log(`App listening on https://localhost:${PORT}`);
-});
+server.listen(PORT, async () => {
+  printConsole(
+      { data: `Server is live @${PORT}` },
+      { printLocation: "index.js:28" },
+      {
+          bgColor: "bgGreen",
+          textColor: "black",
+          underline: true,
+      }
+  )
+})
